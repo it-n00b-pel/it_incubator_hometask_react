86ad5eae01a34b667e42dce9267e36acd0daad00
1t-n00b <mummintrol@gmail.com>
2022-03-10 16:30:10 +0200

    HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import {PATH} from './Pages'
+import s from "./Header.module.css"
 
 
 function Header() {
     return (
-        <div>
-            {/*// add NavLinks*/}
-            <NavLink to={PATH.PRE_JUNIOR}>pre-Junior</NavLink>
-            <NavLink to={PATH.JUNIOR} >Junior |</NavLink>
-            <NavLink to={PATH.JUN_PLUS} >Junior+</NavLink>
-
+        <div className={s.navbar}>
+        <div className={s.menu}>
+            <NavLink to={PATH.PRE_JUNIOR}  className={({isActive}) => (isActive ? s.active : s.item)}>pre-Junior |</NavLink>
+            <NavLink to={PATH.JUNIOR} className={({isActive}) => (isActive ? s.active : s.item)}>Junior |</NavLink>
+            <NavLink to={PATH.JUN_PLUS} className={({isActive}) => (isActive ? s.active : s.item)}>Junior+</NavLink>
         </div>
+        </div>
+
+
+
+
+
+
+
     )
 }
 
